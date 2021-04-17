e261773265dbf173d24f29523b05e4f2ca37dc19
GitHub Action <action@github.com>
2021-04-17 08:17:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/ewinddc/ideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ewinddc/ideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ewinddc.ideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ewinddc.ideaplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/ewinddc/ideaplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/ewinddc/ideaplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.ewinddc.ideaplugin.services
 
+import com.github.ewinddc.ideaplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
